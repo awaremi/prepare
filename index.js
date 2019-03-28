010f539ea5bc2a4ecf3d636ec0c7ef238d2fcd9d
arief wara <arief.wara@gmail.com>
2019-03-28 13:09:20 +0700

    return to plain

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-#! /usr/bin/node
-console.log('hello world');
+#!/usr/bin/env node  
+"use strict";
+const commandLineArgs = require('command-line-args')
+const fetch = require('node-fetch');
+
+const optionDefinitions = [
+    { name: 'verbose', alias: 'v', type: Boolean },
+    { name: 'src', type: String, multiple: true, defaultOption: true },
+    { name: 'timeout', alias: 't', type: Number }
+  ]
+
+const options = commandLineArgs(optionDefinitions)
+
+fetch('http://localhost:8080/test')
+    .then(res => res.text())
+    .then(body => console.log(body));
+
